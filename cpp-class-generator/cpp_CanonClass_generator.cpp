2ca1128280116215c46276c1df86be9f319f708c
lyricall <lachariotte30@gmail.com>
2025-04-10 11:39:39 +0200

    add path

--- cpp-class-generator/cpp_CanonClass_generator.cpp
+++ cpp-class-generator/cpp_CanonClass_generator.cpp
@@ -4,10 +4,31 @@
 #include <string>
 #include <cstdlib> 
 #include <algorithm>
+#include <filesystem>
 
 #define RED "\033[1;31m"
 #define GREEN "\033[1;32m"
+#define YELLOW "\033[1;33m"
 #define RESET "\033[0m"
+
+std::string generateHeader(const std::string& filename) {
+    std::string header;
+
+    header += "╔═════════════════════════════════════════════╗\n";
+    header += "	Class: " + filename+ "\n";
+    header += "╚═════════════════════════════════════════════╝\n\n";
+
+    return header;
+}
+
+void moveFile(const std::string& fileName, const std::string& fileName2, const std::string& destination) {
+    std::string command = "mv " + fileName + " " + fileName2 + " " + destination;
+    int result = std::system(command.c_str());
+    if (result != 0)
+        std::cerr << RED << "❌ Error moving " << fileName << " && "<< fileName2 << " to " << destination << "\n\n" << RESET;
+    else
+        std::cout << GREEN << "✅ File successfully moved from " << fileName << " && "<< fileName2 << " to " << destination << "\n\n" <<RESET;
+}
 
 void writeToCppFile(std::ofstream& cppFile, const std::string& str){
     cppFile << "#include \"" << str << ".hpp\"\n\n";
@@ -20,11 +41,12 @@ void writeToCppFile(std::ofstream& cppFile, const std::string& str){
     cppFile << "}\n\n";
     cppFile << str << "& " << str << "::operator=(const " << str << "& other){\n";
     cppFile << "    if (this != &other) {\n";
+	
     cppFile << "        // copie member\n";
     cppFile << "    }\n";
     cppFile << "    return *this;\n";
     cppFile << "}\n";
-	std::cout << "✅ " <<GREEN << str << ".cpp complete success\n\n" << RESET;
+	std::cout << "✅ " <<GREEN << str << ".cpp complete success\n" << RESET;
 }
 
 void writeToHppFile(std::ofstream& hppFile, const std::string& str){
@@ -47,8 +69,9 @@ void writeToHppFile(std::ofstream& hppFile, const std::string& str){
 }
 
 
-void	ArgToFile(const std::string& str){
+void	ArgToFile(const std::string& str, const std::string& destination){
 
+	std::cout << YELLOW << generateHeader(str) << RESET;
 	std::ofstream	cppFile((str + ".cpp"));
 	if (!cppFile.is_open()){
 		std::cerr << RED << "❌ " << str << ".cpp cannot create correctly" << RESET << std::endl;
@@ -61,19 +84,42 @@ void	ArgToFile(const std::string& str){
 	std::ofstream	hppFile((str + ".hpp"));
 	if (!hppFile.is_open()){
 		std::cerr << RED << "❌ " << str << ".hpp cannot create correctly" << RESET << std::endl;
+		cppFile.close();
 		std::exit(1);
 	}
-	std::cout << "✅ " <<GREEN << str << ".hpp correctly create !\n" << RESET;
+	std::cout << "✅ " <<GREEN << str << ".hpp correctly create . . .\n" << RESET;
 	writeToHppFile(hppFile, str);
 	hppFile.close();
+
+	if (std::filesystem::current_path() != destination)
+    	moveFile((str + ".cpp"), (str + ".hpp"), destination);
 }
 
 int main(int argc, char **argv){
-	if (argc == 1){
-		std::cerr << RED << "❌ Enter one or more ClassName in argument\n❌ Usage: " << argv[0] << " ClassName ClassName1 ...\n" << RESET;
+	if (argc < 3){
+		std::cerr << RED << "❌ Enter one or more ClassName in argument\n❌ Usage: " << argv[0] << " /path ClassName ClassName1 ...\n" << RESET;
 		return 1;
 	}
-	for (int i = 1; argv[i]; i++)
-		ArgToFile(std::string (argv[i]));
+	std::string destination = argv[1];
+	if (destination.empty() || destination == "." || destination == "./") {
+        destination = std::filesystem::current_path();
+    }
+	if (!std::filesystem::exists(destination) || !std::filesystem::is_directory(destination)) {
+		std::cerr << RED << "❌ " << destination << " doesn't exist. Do you want to create it?\n(yes/no): " << RESET << std::endl;
+		std::string userResponse;
+		std::getline(std::cin, userResponse);
+		if (userResponse == "yes"){
+			if (std::filesystem::create_directory(destination))
+				std::cout << GREEN << "✅ Directory created successfully." << RESET << std::endl;
+			else{
+				std::cerr << RED << "❌ Failed to create directory." << RESET << std::endl;
+				return 1;
+			}
+    	}
+		else
+			return 1;
+	}
+	for (int i = 2; argv[i]; i++)
+		ArgToFile(std::string (argv[i]), destination);
 	return 0;
 }
